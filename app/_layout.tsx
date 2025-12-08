2141315cc3695db22b79962bdbc665e59e5858dc
greggyb310 <greg@natureuphealth.com>
2025-12-08 11:26:09 -0700

    Updated _layout.tsx

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,7 +1,10 @@
 import { Stack } from 'expo-router';
 import { StatusBar } from 'expo-status-bar';
+import { useFrameworkReady } from '@/hooks/useFrameworkReady';
 
 export default function RootLayout() {
+  useFrameworkReady();
+
   return (
     <>
       <Stack screenOptions={{ headerShown: false }}>
